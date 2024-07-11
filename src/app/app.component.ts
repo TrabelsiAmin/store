73efb7195c96a1a568b2eb22498811eff9192914
TrabelsiAmin <medaminetr30@gmail.com>
2024-07-11 14:15:00 +0100

    simple changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <app-header></app-header>
 
-    <router-outlet />
+    
   `,
   styles: []
 })
